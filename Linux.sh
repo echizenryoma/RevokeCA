51d01305ef2e13aa512b3f86af24fe6a9250539c
Echizen Ryoma <echizenryoma.zhang@gmail.com>
2023-03-21 22:43:51 +0800

    add CFCA_EV_ROOT.crt

--- Linux.sh
+++ Linux.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mkdir -p /etc/ca-certificates/trust-source/blocklist
-crt_files=$(ls -1 ./crt/*)
+base_dir=$(realpath $(dirname $0))
+mkdir -p "/etc/ca-certificates/trust-source/blocklist"
+crt_files=$(ls -1 "${base_dir}"/crt/*)
 for crt_file in ${crt_files}
 do
     cer_name="$(basename ${crt_file})"
     pem_name="${cer_name%%.*}.pem"
-    openssl x509 -inform der -in "$ca_file" -outform pem > "/etc/ca-certificates/trust-source/blocklist/${pem_name}"
+    openssl x509 -inform der -in "${crt_file}" -outform pem > "/etc/ca-certificates/trust-source/blocklist/${pem_name}"
 done
-update-ca-trust
